158effbcc16f616bf691a4ed746e0e186d716335
Gerardo Capiel <gcapiel@alum.mit.edu>
2013-11-04 14:08:40 -0800

    fixes for switches in daylight savings time

--- getbus.rb
+++ getbus.rb
@@ -1,13 +1,20 @@
 #!/usr/bin/env ruby
+# encoding: utf-8
 
 require 'muni'
 require 'viewpoint'
+require 'tzinfo'
 include Viewpoint::EWS
+
 Viewpoint::EWS::EWS.endpoint = 'https://pod51043.outlook.com/ews/Exchange.asmx'
 ExchAccount = File.read("/root/exchcred.txt")
 
 Viewpoint::EWS::EWS.set_auth(ExchAccount.split("|")[0],ExchAccount.split("|")[1])
- 
+
+def adjust_time time, time_zone="America/Los_Angeles"
+    return TZInfo::Timezone.get(time_zone).utc_to_local(time.utc)
+end
+
 def replaceTimes(sourceCode, index)
 
 	muniLine = ARGV[0].scan(/[0-9]+/)[0]
@@ -15,20 +22,21 @@ def replaceTimes(sourceCode, index)
 	# if muniLine is zero, then we don't want bus times to be checked and included in watch app
 	if muniLine != "0" then
 		r = ARGV[0].scan(/[0-9]+/)[2]=="1" ? Muni::Route.find(muniLine).inbound : Muni::Route.find(muniLine).outbound
-		nextTime = Time.at(r.stop_at(ARGV[0].scan(/[0-9]+/)[1]).predictions.map(&:epochTime)[index].to_i/1000).utc + Time.zone_offset("PDT")
+		nextTime = adjust_time(Time.at(r.stop_at(ARGV[0].scan(/[0-9]+/)[1]).predictions.map(&:epochTime)[index].to_i/1000).utc)
 		sourceCode = sourceCode.gsub(/\*\*YEAR#{index}\*\*/, nextTime.year.to_s())
 		sourceCode = sourceCode.gsub(/\*\*MONTH#{index}\*\*/, nextTime.month.to_s())
 		sourceCode = sourceCode.gsub(/\*\*DAY#{index}\*\*/, nextTime.mday.to_s())
 		sourceCode = sourceCode.gsub(/\*\*HOUR#{index}\*\*/, nextTime.hour.to_s())
 		sourceCode = sourceCode.gsub(/\*\*MIN#{index}\*\*/, nextTime.min.to_s())
 	else
-		sourceCode = sourceCode.gsub(/\*\*YEAR#{index}\*\*/, "2000")
+		sourceCode = sourceCode.gsub(/\*\*YEAR#{index}\*\*/, "2000")	
 		sourceCode = sourceCode.gsub(/\*\*MONTH#{index}\*\*/, "1")
 		sourceCode = sourceCode.gsub(/\*\*DAY#{index}\*\*/, "1")
 		sourceCode = sourceCode.gsub(/\*\*HOUR#{index}\*\*/, "0")
-		sourceCode = sourceCode.gsub(/\*\*MIN#{index}\*\*/, "0")		
+		sourceCode = sourceCode.gsub(/\*\*MIN#{index}\*\*/, "0")
+
 	end
-  
+	
   return sourceCode
 
 end
@@ -38,15 +46,15 @@ def insertSchedule(sourceCode)
 	eventCode = ""
 	calendar = Viewpoint::EWS::CalendarFolder.get_folder :calendar
 	
-	currentYear = (DateTime.now.to_time.utc + Time.zone_offset("PDT")).to_datetime.year()
-	currentMon = (DateTime.now.to_time.utc + Time.zone_offset("PDT")).to_datetime.mon()
-	currentDay = (DateTime.now.to_time.utc + Time.zone_offset("PDT")).to_datetime.day()
+	currentYear = adjust_time(Time.now).to_datetime.year()
+	currentMon = adjust_time(Time.now).to_datetime.mon()
+	currentDay = adjust_time(Time.now).to_datetime.day()
 	
 	todaysItems = calendar.items_between(DateTime.new(currentYear,currentMon,currentDay,0,0,0,'-7'),DateTime.new(currentYear,currentMon,currentDay,23,59,59,'-7'))
 	
 	todaysItems.each_with_index do |calEvent, index|
 		eventCode = eventCode + "cal_event = &cal_events[#{index}];\n"
-		eventStart = calEvent.start.to_time.utc + Time.zone_offset("PDT")
+		eventStart = adjust_time(calEvent.start.to_time.utc)
 		eventCode = eventCode + "cal_event->eventTitle = \"#{eventStart.hour()}:%02d #{calEvent.subject}\\n\";\n" % eventStart.min()
 		eventStartMin = (eventStart.year()-1900)*365*24*60 + eventStart.month()*31*24*60 + eventStart.day()*24*60 + eventStart.hour()*60 + eventStart.min()
 		eventCode = eventCode + "cal_event->eventTimeMin = #{eventStartMin};\n"
@@ -66,7 +74,7 @@ def insertSchedule(sourceCode)
   
 end
 
-sourceCode = File.read("/root/BusTimer/src/example_template.txt")
+sourceCode = File.read("/root/BusTimer/src/example_template.txt", :encoding => "UTF-8")
 
 sourceCode = replaceTimes(sourceCode, 0)
 sourceCode = replaceTimes(sourceCode, 1)
